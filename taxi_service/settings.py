79e55a20cb7b920b6906fbaefc65b178b4ee921c
Rafael Carvalho <dannyelrc@hotmail.com>
2026-09-21 16:28:57 -0300

    Solution 2

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "crispy_forms",
-    "crispy_bootstrap4",
     "taxi",
 ]
 
@@ -78,7 +77,7 @@ TEMPLATES = [
 ]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
 
 WSGI_APPLICATION = "taxi_service.wsgi.application"
 
