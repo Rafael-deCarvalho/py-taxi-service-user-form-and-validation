a543628388685a811dd91dcd2462afca29216f6e
Rafael Carvalho <dannyelrc@hotmail.com>
2026-09-21 16:44:06 -0300

    Last Bugs fixed

--- taxi/views.py
+++ taxi/views.py
@@ -66,13 +66,13 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
 
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
